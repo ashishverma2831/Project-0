ca817cbfa2fb835260e108cc96454d0341ee0c94
ashishverma2831 <ashishverma510207@gmail.com>
2024-03-24 18:20:58 +0530

    Add MongoDB connection and enable CORS

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+
+mongoose.connect(process.env.MONGO_DB_URI || process.env.PORT)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
+
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div className='bg-red-400'>App</div>
+    <div className='bg-red-400'>
+      <Login />
+    </div>
   )
 }
 
