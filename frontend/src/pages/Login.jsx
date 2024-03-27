897df592c27b4123ad7d4fc99419d7509421963e
ashishverma2831 <ashishverma510207@gmail.com>
2024-03-27 22:19:13 +0530

    Fix login page layout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
         <>
             <section className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <a
+                    {/* <a
                         href="#"
                         className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
                     >
@@ -15,11 +15,11 @@ const Login = () => {
                             alt="logo"
                         />
                         Flowbite
-                    </a>
+                    </a> */}
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Sign in to your account
+                            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Sign in 
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
@@ -54,7 +54,7 @@ const Login = () => {
                                         required=""
                                     />
                                 </div>
-                                <div className="flex items-center justify-between">
+                                {/* <div className="flex items-center justify-between">
                                     <div className="flex items-start">
                                         <div className="flex items-center h-5">
                                             <input
@@ -80,14 +80,14 @@ const Login = () => {
                                     >
                                         Forgot password?
                                     </a>
-                                </div>
+                                </div> */}
                                 <button
                                     type="submit"
-                                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                    className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                 >
                                     Sign in
                                 </button>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                {/* <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Don’t have an account yet?{" "}
                                     <a
                                         href="#"
@@ -95,7 +95,7 @@ const Login = () => {
                                     >
                                         Sign up
                                     </a>
-                                </p>
+                                </p> */}
                             </form>
                         </div>
                     </div>
