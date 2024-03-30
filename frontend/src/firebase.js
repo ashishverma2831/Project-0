24cbbd1bb191acf2b348aaf769f3e968f222d6d1
ashishverma2831 <ashishverma510207@gmail.com>
2024-03-30 21:14:39 +0530

    Add dotenv package and update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,13 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.VITE_FIREBASE_KEY);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC12kCkKe5ThKFWez1WJ50w5YjW2xoX-X0",
+  apiKey: process.env.VITE_FIREBASE_KEY,
   authDomain: "project0-9e848.firebaseapp.com",
   projectId: "project0-9e848",
   storageBucket: "project0-9e848.appspot.com",
@@ -17,5 +24,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
