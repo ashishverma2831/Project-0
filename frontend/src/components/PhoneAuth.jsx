71b39578b5f23a39389963bd745f0829036d08d2
ashishverma2831 <ashishverma510207@gmail.com>
2024-04-01 11:40:09 +0530

    Add phone number authentication using Firebase auth

--- frontend/src/components/PhoneAuth.jsx
+++ frontend/src/components/PhoneAuth.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
 
 const PhoneAuth = () => {
 
     const handlePhoneAuth = async () => {
         // console.log('Phone Auth');
-        
+        const auth = getAuth();
+        window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
+            'size': 'invisible',
+            'callback': (response) => {
+                // reCAPTCHA solved, allow signInWithPhoneNumber.
+                onSignInSubmit();
+            }
+        });
     }
 
-  return (
-    <>
-        <button onClick={handlePhoneAuth} className='bg-green-500 text-white w-full rounded-lg py-2 hover:opacity-90'>Sign in with the Phone number</button>
-    </>
-  )
+    return (
+        <>
+            <button onClick={handlePhoneAuth} className='bg-green-500 text-white w-full rounded-lg py-2 hover:opacity-90'>Sign in with the Phone number</button>
+        </>
+    )
 }
 
 export default PhoneAuth
