80ccddd5a9c05f5e80e712e7accc558011ee1a87
ashishverma2831 <ashishverma510207@gmail.com>
2024-04-03 21:21:18 +0530

    Add multer package and implement file upload form

--- frontend/src/pages/Multer.jsx
+++ frontend/src/pages/Multer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Multer = () => {
-  return (
-    <div>Multer</div>
-  )
+    return (
+        <>
+            <form action="/profile" method="post" enctype="multipart/form-data">
+                <input type="file" name="avatar" />
+            </form>
+        </>
+    )
 }
 
 export default Multer
