3a1574ef4635060c0892d86e46537b9b539a9e46
ashishverma2831 <ashishverma510207@gmail.com>
2024-04-29 11:20:57 +0530

    Refactor cloudinary upload functionality and remove commented code

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -6,6 +6,34 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
+
+
+
+
+
+
+
+// const uploadOnCloudinary = async (localFilePath) => {
+//     try {
+//         if (!localFilePath) return null
+//         //upload the file on cloudinary
+//         const response = await cloudinary.uploader.upload(localFilePath, {
+//             resource_type: "auto"
+//         })
+//         // file has been uploaded successfull
+//         //console.log("file is uploaded on cloudinary ", response.url);
+//         fs.unlinkSync(localFilePath)
+//         return response;
+
+//     } catch (error) {
+//         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+//         return null;
+//     }
+// }
+
+
+
+// export {uploadOnCloudinary}
