0178441d7226241c73a9b64f67a9dfb9ca3c5d7c
ashishverma2831 <ashishverma510207@gmail.com>
2024-04-30 19:35:31 +0530

    Refactor cloudinary configuration and enable secure mode in cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -4,13 +4,14 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config({ 
   cloud_name: 'dof7hiljv', 
   api_key: '176618429877884', 
-  api_secret: '***************************' 
+  api_secret: '***************************' ,
+  secure: true
 });
 
 
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
 
 
 
@@ -24,54 +25,54 @@ cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/
 // });
 
 // Log the configuration
-// console.log(cloudinary.config());
+console.log(cloudinary.config());
 
 
 
 /////////////////////////
 // Uploads an image file
 /////////////////////////
-// const uploadImage = async (imagePath) => {
+const uploadImage = async (imagePath) => {
 
-//   // Use the uploaded file's name as the asset's public ID and 
-//   // allow overwriting the asset with new versions
-//   const options = {
-//     use_filename: true,
-//     unique_filename: false,
-//     overwrite: true,
-//   };
+  // Use the uploaded file's name as the asset's public ID and 
+  // allow overwriting the asset with new versions
+  const options = {
+    use_filename: true,
+    unique_filename: false,
+    overwrite: true,
+  };
 
-//   try {
-//     // Upload the image
-//     const result = await cloudinary.uploader.upload(imagePath, options);
-//     console.log(result);
-//     return result.public_id;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+  try {
+    // Upload the image
+    const result = await cloudinary.uploader.upload(imagePath, options);
+    console.log(result);
+    return result.public_id;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 
  
 /////////////////////////////////////
 // Gets details of an uploaded image
 /////////////////////////////////////
-// const getAssetInfo = async (publicId) => {
+const getAssetInfo = async (publicId) => {
 
-//   // Return colors in the response
-//   const options = {
-//     colors: true,
-//   };
+  // Return colors in the response
+  const options = {
+    colors: true,
+  };
 
-//   try {
-//       // Get details about the asset
-//       const result = await cloudinary.api.resource(publicId, options);
-//       console.log(result);
-//       return result.colors;
-//       } catch (error) {
-//       console.error(error);
-//   }
-// };
+  try {
+      // Get details about the asset
+      const result = await cloudinary.api.resource(publicId, options);
+      console.log(result);
+      return result.colors;
+      } catch (error) {
+      console.error(error);
+  }
+};
 
 
 
@@ -81,23 +82,23 @@ cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/
 // focused on the faces, applying an outline of the  
 // first color, and setting a background of the second color.
 // //////////////////////////////////////////////////////////////
-// const createImageTag = (publicId, ...colors) => {
+const createImageTag = (publicId, ...colors) => {
     
-//   // Set the effect color and background color
-//   const [effectColor, backgroundColor] = colors;
+  // Set the effect color and background color
+  const [effectColor, backgroundColor] = colors;
 
-//   // Create an image tag with transformations applied to the src URL
-//   let imageTag = cloudinary.image(publicId, {
-//     transformation: [
-//       { width: 250, height: 250, gravity: 'faces', crop: 'thumb' },
-//       { radius: 'max' },
-//       { effect: 'outline:10', color: effectColor },
-//       { background: backgroundColor },
-//     ],
-//   });
+  // Create an image tag with transformations applied to the src URL
+  let imageTag = cloudinary.image(publicId, {
+    transformation: [
+      { width: 250, height: 250, gravity: 'faces', crop: 'thumb' },
+      { radius: 'max' },
+      { effect: 'outline:10', color: effectColor },
+      { background: backgroundColor },
+    ],
+  });
 
-//   return imageTag;
-// };
+  return imageTag;
+};
 
 
 
@@ -109,24 +110,24 @@ cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/
 // Main function
 //
 //////////////////
-// (async () => {
+(async () => {
 
-//   // Set the image to upload
-//   const imagePath = 'https://cloudinary-devs.github.io/cld-docs-assets/assets/images/happy_people.jpg';
+  // Set the image to upload
+  const imagePath = 'https://cloudinary-devs.github.io/cld-docs-assets/assets/images/happy_people.jpg';
 
-//   // Upload the image
-//   const publicId = await uploadImage(imagePath);
+  // Upload the image
+  const publicId = await uploadImage(imagePath);
 
-//   // Get the colors in the image
-//   const colors = await getAssetInfo(publicId);
+  // Get the colors in the image
+  const colors = await getAssetInfo(publicId);
 
-//   // Create an image tag, using two of the colors in a transformation
-//   const imageTag = await createImageTag(publicId, colors[0][0], colors[1][0]);
+  // Create an image tag, using two of the colors in a transformation
+  const imageTag = await createImageTag(publicId, colors[0][0], colors[1][0]);
 
-//   // Log the image tag to the console
-//   console.log(imageTag);
+  // Log the image tag to the console
+  console.log(imageTag);
 
-// })();
+})();
 
  
 
